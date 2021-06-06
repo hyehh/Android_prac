f7678518ae7c51650d14294d9b4de82dad3b3d6c
hyehh <81559324+hyehh@users.noreply.github.com>
2021-06-06 10:43:16 +0900

    2021.06.06 First Commit

--- GridViewPoster1/app/src/main/java/com/aoslec/gridviewposter1/MainActivity.java
+++ GridViewPoster1/app/src/main/java/com/aoslec/gridviewposter1/MainActivity.java
@@ -3,12 +3,17 @@ package com.aoslec.gridviewposter1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    GridView gridView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
 }
